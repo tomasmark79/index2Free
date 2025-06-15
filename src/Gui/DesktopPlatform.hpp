b6865dc8f05d45f232151446e901f4202ba5abbb
tomasmark79 <admin@digitalspace.name>
2025-06-15 19:32:33 +0200

    cleaned redundancy

--- src/Gui/DesktopPlatform.hpp
+++ src/Gui/DesktopPlatform.hpp
@@ -4,12 +4,15 @@
 #include "PlatformManager.hpp"
 
 class DesktopPlatform : public PlatformManager {
+
+  float userConfigurableScale_ = 2.0f;
+
 public:
   DesktopPlatform () = default;
   ~DesktopPlatform () override = default;
 
   void initialize () override;
-  void shutdown() override;
+  void shutdown () override;
   void createSDL2Window (const char* title, int width, int height) override;
   void updateWindowSize () override;
   void createOpenGLContext () override;
@@ -18,12 +21,8 @@ public:
   void setupShaders () override;
   GLuint compileShader (const char* shaderSource, GLenum shaderType) override;
   void renderBackground (float deltaTime) override;
-
   void initializeImGui () override;
   void scaleImGui () override;
-
-private:
-  float userConfigurableScale_ = 2.0f;
 };
 
 #endif
