881b4ca4886f03c5df91bf7c70ad90251afeff15
tomasmark79 <tomas@digitalspace.name>
2025-06-19 09:17:02 +0200

    no good method to hardcode strings

--- src/Gui/GuiStrings.hpp
+++ src/Gui/GuiStrings.hpp
@@ -7,8 +7,7 @@
 // Just one hardcoded string for the entire application
 // Anothers are going to be loaded from a json file
 
-inline constexpr std::string_view kAboutWebsite
-    = R"(no good method to hardcode strings)";
+inline constexpr std::string_view kAboutWebsite = R"(no good method to hardcode strings)";
 
 // Function to convert a string_view to a string
 inline std::string to_string (std::string_view sv) {
