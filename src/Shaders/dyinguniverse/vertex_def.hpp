eb3209a1ac4cef2f34f6ded660c38e508db6f395
tomasmark79 <tomas@digitalspace.name>
2025-07-18 15:32:12 +0200

    added more fragment shaders as examples

--- src/Shaders/dyinguniverse/vertex_def.hpp
+++ src/Shaders/dyinguniverse/vertex_def.hpp
@@ -5,7 +5,9 @@
 const char* vertexShader200 = R"(
 precision highp float;
 attribute vec2 pos;
+varying vec2 uv;
 void main() {
+   uv = pos * 0.5 + 0.5;
    gl_Position = vec4(pos, 0.0, 1.0);
 }
 )";
@@ -14,7 +16,9 @@ void main() {
 const char* vertexShader300 = R"(#version 300 es
 precision highp float;
 layout (location = 0) in vec2 pos;
+out vec2 uv;
 void main() {
+   uv = pos * 0.5 + 0.5;
    gl_Position = vec4(pos, 0.0, 1.0);
 }
 )";
@@ -22,7 +26,9 @@ void main() {
 // Desktop OpenGL shaders (OpenGL 3.3)
 const char* vertexShader330 = R"(#version 330 core
 layout (location = 0) in vec2 pos;
+out vec2 uv;
 void main() {
+   uv = pos * 0.5f + 0.5f;
    gl_Position = vec4(pos, 0.0f, 1.0f);
 }
 )";
