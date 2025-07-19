241161ae2820e9e6c3b5f4f454f29c30001b1950
tomasmark79 <tomas@digitalspace.name>
2025-07-19 12:12:04 +0200

    exclude emscripten from strip debug

--- cmake/tmplt-runtime.cmake
+++ cmake/tmplt-runtime.cmake
@@ -1,17 +1,51 @@
-# MIT License
-# Copyright (c) 2024-2025 Tomáš Mark
+# MIT License Copyright (c) 2024-2025 Tomáš Mark
+
+# !!! -lstdc++ mus not be used in the final link command
+# build/standalone/default/debug/CMakeFiles/index2.dir/link.txt
 
 # ==============================================================================
-# use Static runtime
+# Static Runtime Configuration (SIMPLIFIED)
 # ==============================================================================
-if(USE_STATIC_RUNTIME)
-    if(MSVC)
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-    else()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libstdc++ -static-libgcc")
-    endif()
-    
-    if(UNIX)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -pthread")
-    endif()
+
+# Define option with default value (safe to call multiple times)
+if(NOT DEFINED USE_STATIC_RUNTIME)
+    option(USE_STATIC_RUNTIME "Link against static runtime libraries" OFF)
 endif()
+
+# ==============================================================================
+# Apply static runtime to target (does everything needed)
+# ==============================================================================
+function(apply_static_runtime TARGET_NAME)
+    if(USE_STATIC_RUNTIME)
+        message(STATUS "Applying static runtime to: ${TARGET_NAME}")
+
+        if(MSVC)
+            # MSVC runtime library property
+            set_property(TARGET ${TARGET_NAME} PROPERTY MSVC_RUNTIME_LIBRARY
+                                                        "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+        else()
+            if(WIN32)
+                # MinGW - full static linking
+                target_link_options(${TARGET_NAME} PRIVATE -static)
+            elseif(NOT CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
+                # Linux/macOS - static C++ runtime only (exclude Emscripten)
+                target_link_options(${TARGET_NAME} PRIVATE -static-libgcc -static-libstdc++
+                                    -Wl,--as-needed)
+            endif()
+        endif()
+
+        # For executables, prefer static libraries
+        # NOTE: If some dependencies are only available as shared libraries (.so),
+        # comment out the following lines to avoid linking errors.
+        # Alternatively, you can selectively disable static linking for specific targets.
+        # Example: Force shared linking for zlib if only .so is available:
+        #   find_package(ZLIB REQUIRED)
+        #   set_target_properties(ZLIB::ZLIB PROPERTIES IMPORTED_LINK_INTERFACE_LIBRARIES "")
+        #   target_link_libraries(${TARGET_NAME} PRIVATE ${ZLIB_LIBRARIES})
+        #get_target_property(TARGET_TYPE ${TARGET_NAME} TYPE)
+        #if(TARGET_TYPE STREQUAL "EXECUTABLE")
+        #    set_target_properties(${TARGET_NAME} PROPERTIES LINK_SEARCH_START_STATIC ON
+        #                                                    LINK_SEARCH_END_STATIC ON)
+        #endif()
+    endif()
+endfunction()
